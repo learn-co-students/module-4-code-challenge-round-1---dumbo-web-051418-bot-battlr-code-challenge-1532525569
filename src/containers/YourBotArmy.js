9c15173933ee98285b2f7458469b1392c19633fd
Jee Graeff <jag918@nyu.edu>
2018-07-25 11:04:38 -0400

    challenge

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -4,19 +4,29 @@ import BotCard from "../components/BotCard";
 class YourBotArmy extends React.Component {
   //your bot army code here...
 
+  renderArmyBots = () => {
+    this.props.bots.map((bot) => {
+      console.log(bot)
+
+      // trying to figure out why my botCard is not displaying
+      // i called the renderArmyBots function inside the div so not sure why its not
+      // adding the BotCard 
+      return <BotCard bot={bot} key = {bot.id}/>
+    })
+  }
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.renderArmyBots()}
+
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
